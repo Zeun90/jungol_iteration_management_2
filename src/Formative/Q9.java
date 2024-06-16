51855c44f97933e5a0bf6b00d94e3d106dd447ad
Lee JiEun <gsilver190@gmail.com>
2024-06-17 01:24:25 +0900

    형성 평가 9

---
+++ src/Formative/Q9.java
@@ -0,0 +1,19 @@
+package Formative;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q9 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num; j++) {
+                System.out.printf("(%d, %d) ", i, j);
+            }
+            System.out.println();
+        }
+    }
+}
