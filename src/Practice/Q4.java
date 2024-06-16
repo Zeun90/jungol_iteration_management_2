f584de5a7cc2a6ee357ef77195a9e2f0758891df
Lee JiEun <gsilver190@gmail.com>
2024-06-16 23:47:40 +0900

    연습 문제 4

---
+++ src/Practice/Q4.java
@@ -0,0 +1,20 @@
+package Practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for (int i = 1; i <= input; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+
+    }
+}
