5b30837fa667f5ccf72370712a2a8edafeea8fb0
Lee JiEun <gsilver190@gmail.com>
2024-06-16 23:38:43 +0900

    자가 진단 2

---
+++ src/SelfAssessment/Q2.java
@@ -0,0 +1,9 @@
+package SelfAssessment;
+
+public class Q2 {
+    public static void main(String[] args) {
+        for (int i = 10; i <= 20; i++) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
