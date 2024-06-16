c0fa7e473f07c7eb59e0962fd777dc04ae73d715
Lee JiEun <gsilver190@gmail.com>
2024-06-16 23:40:37 +0900

    연습 문제 3

---
+++ src/Practice/Q3.java
@@ -0,0 +1,9 @@
+package Practice;
+
+public class Q3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i+=2) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
