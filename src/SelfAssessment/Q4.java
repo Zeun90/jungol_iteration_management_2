98c2138fbe49f70fa9930af837bce78222889013
Lee JiEun <gsilver190@gmail.com>
2024-06-16 23:52:18 +0900

    자가 진단 4

---
+++ src/SelfAssessment/Q4.java
@@ -0,0 +1,22 @@
+package SelfAssessment;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        if (0 < input && input <= 100) {
+            for (int i = 1; i <= input; i++) {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+
+    }
+}
