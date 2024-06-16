7653d042f745a9bf05d42dd7b5e27a04b1a1c991
Lee JiEun <gsilver190@gmail.com>
2024-06-17 01:08:56 +0900

    형성 평가 4

---
+++ src/Formative/Q4.java
@@ -0,0 +1,27 @@
+package Formative;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int cnt = Integer.parseInt(st.nextToken());
+        int sum = 0;
+        double avg;
+
+        if (1 <= cnt && cnt <= 100) {
+            for (int i = 0; i < cnt; i++) {
+                st = new StringTokenizer(br.readLine());
+                sum += Integer.parseInt(st.nextToken());
+            }
+            avg = (double) sum / cnt;
+
+            System.out.printf("%.2f", avg);
+        }
+    }
+}
